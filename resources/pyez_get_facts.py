0083553742479f7dc8e4e759153ad944f14105c1
root <root@dc13-cfg-1.(none)>
2015-06-18 12:43:30 -0700

    Update-3

--- resources/pyez_get_facts.py
+++ resources/pyez_get_facts.py
@@ -4,7 +4,6 @@ from heat.db import api as db_api
 from heat.engine import properties
 from heat.engine import resource
 from jnpr.junos import Device
-from pprint import pprint
 
 
 class GetFacts(resource.Resource):
@@ -47,6 +46,16 @@ class GetFacts(resource.Resource):
         'hostname': _("target name."),
         'model': _("target model."),
         'serialnumber': _("serial number."),
+        'HOME': _("HOME."),
+        'reboot_reason': _("last reboot reason."),
+        'status': _("status."),
+        'up_time': _("up time."),
+        'domain': _("domain."),
+        'fqdn': _("fqdn."),
+        'ifd_style': _("ifd_style."),
+        'personality': _("personality."),
+        'switch_style': _("switch style."),
+        'version': _("version."),
     }
 
     def handle_create(self):
@@ -55,6 +64,16 @@ class GetFacts(resource.Resource):
         db_api.resource_data_set(self, 'hostname', dev.facts['hostname'], redact=True)
         db_api.resource_data_set(self, 'model', dev.facts['model'], redact=True)
         db_api.resource_data_set(self, 'serialnumber', dev.facts['serialnumber'], redact=True)
+        db_api.resource_data_set(self, 'HOME', dev.facts['HOME'], redact=True)
+        db_api.resource_data_set(self, 'reboot_reason', dev.facts['RE0']['last_reboot_reason'], redact=True)
+        db_api.resource_data_set(self, 'status', dev.facts['RE0']['status'], redact=True)
+        db_api.resource_data_set(self, 'up_time', dev.facts['RE0']['up_time'], redact=True)
+        db_api.resource_data_set(self, 'domain', dev.facts['domain'], redact=True)
+        db_api.resource_data_set(self, 'fqdn', dev.facts['fqdn'], redact=True)
+        db_api.resource_data_set(self, 'ifd_style', dev.facts['ifd_style'], redact=True)
+        db_api.resource_data_set(self, 'personality', dev.facts['personality'], redact=True)
+        db_api.resource_data_set(self, 'switch_style', dev.facts['switch_style'], redact=True)
+        db_api.resource_data_set(self, 'version', dev.facts['version'], redact=True)
         dev.close()  
 
     def handle_delete(self):
@@ -65,11 +84,32 @@ class GetFacts(resource.Resource):
 
     def _resolve_attribute(self, name):
         if name == 'hostname':
-          return db_api.resource_data_get(self, 'hostname')
+            return db_api.resource_data_get(self, 'hostname')
         elif name == 'model':
-          return db_api.resource_data_get(self, 'model')
+            return db_api.resource_data_get(self, 'model')
         elif name == 'serialnumber':
-          return db_api.resource_data_get(self, 'serialnumber')
+            return db_api.resource_data_get(self, 'serialnumber')
+        elif name == 'HOME':
+            return db_api.resource_data_get(self, 'HOME')
+        elif name == 'reboot_reason':
+            return db_api.resource_data_get(self, 'reboot_reason')
+        elif name == 'status':
+            return db_api.resource_data_get(self, 'status')
+        elif name == 'up_time':
+            return db_api.resource_data_get(self, 'up_time')
+        elif name == 'domain':
+            return db_api.resource_data_get(self, 'domain')
+        elif name == 'fqdn':
+            return db_api.resource_data_get(self, 'fqdn')
+        elif name == 'ifd_style':
+            return db_api.resource_data_get(self, 'ifd_style')
+        elif name == 'personality':
+            return db_api.resource_data_get(self, 'personality')
+        elif name == 'switch_style':
+            return db_api.resource_data_get(self, 'switch_style')
+        elif name == 'version':
+            return db_api.resource_data_get(self, 'version')
+        
 
 def resource_mapping():
     return {
